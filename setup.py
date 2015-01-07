00191a7875cb9415b66f6d393b49a9b9c3d47a4a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-01-07 13:12:59 +0100

    remove outdated/unneded metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-import os
-
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'clld>=0.19.1',
@@ -20,7 +15,7 @@ requires = [
 setup(name='sails',
       version='0.0',
       description='sails',
-      long_description=README + '\n\n' + CHANGES,
+      long_description='',
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
